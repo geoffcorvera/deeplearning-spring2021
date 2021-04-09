209b46727fdcd5d8c0f909516541efde3edda584
Geoff Corvera <geoffcorvera@gmail.com>
2021-04-09 13:01:14 -0700

    remove old gaussian filter .py

--- p1/gaussianFilter.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-# import matplotlib.image as mpimg
-
-# see image tutorial: https://matplotlib.org/2.0.0/users/image_tutorial.html
-
-x = np.linspace(0, 20, 100)
-plt.plot(x, np.sin(x))
-plt.show()
