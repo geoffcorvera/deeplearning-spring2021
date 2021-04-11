1d191bf0b7f7b8560dd0f5af67015c53ea345a56
Geoff Corvera <geoffcorvera@gmail.com>
2021-04-11 14:59:30 -0700

    fix implementation of convolve2d

--- p1/gaussFilter.py
+++ p1/gaussFilter.py
@@ -19,25 +19,27 @@ img2 = image.imread('filter2_img.jpg')
 # - img1: 512x512, 8-Bit image
 # - img2: 530x800, 3-channel 8-bit 
 
-# %%
-plt.imshow(img1, cmap='gray')
-plt.show()
-plt.imshow(img2)
-plt.show()
-
-# %% [markdown]
 # ## Apply Gaussian Blur
 # Convolve the discretized Gaussian filters (3x3 & 5x5) with the input images. Use zero-pad width of 1 for 3x3 filter, and width 2 for 5x5, and *stride* = 1.
 
 # %%
-def convolve2D(image, kernel, padding, stride=1):
-    outw = 1 + (image.shape[0] - kernel.shape[0] + 2*padding) / stride
-    outh = 1 + (image.shape[1] - kernel.shape[1] + 2*padding) / stride
-    img = padImage(image, padding)
+def convolve2D(img, kernel, padding, stride=1):
+    assert kernel.shape[0] == kernel.shape[1]
+    k = kernel.shape[0]
+    outw = int(1 + (img.shape[0] - k + 2*padding) / stride)
+    outh = int(1 + (img.shape[1] - k + 2*padding) / stride)
+    output = np.zeros((outw, outh))
     
-    # TODO: implement discretized conovolution
+    if (padding != 0):
+        img = padImage(img, padding)
 
-    return img
+    # Iterate through image & multiply kernel
+    for y in range(img.shape[1]-k):
+        for x in range(img.shape[0]-k):
+            output[x,y] = np.sum(img[x:x+k, y:y+k] * kernel)
+    
+    return output
+
 
 def padImage(image, pwidth=1):
     rowpad = np.zeros((1,image.shape[1]))
