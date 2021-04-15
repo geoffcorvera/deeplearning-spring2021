7b063e6bf8ec44c99ed8c2a1d1fdeff60f104afc
Geoffrey Corvera <geoffcorvera@gmail.com>
2021-04-14 19:07:46 -0700

    implement kmeans cluster assignment

--- kmean.py
+++ kmean.py
@@ -6,6 +6,8 @@ data = np.genfromtxt('hw1/510_cluster_dataset.txt')
 plt.scatter(data[:, :1], data[:, 1:])
 plt.show()
 
+
+# %%
 class KMeans(object):
 
     def __init__(self, X, k):
@@ -14,3 +16,16 @@ class KMeans(object):
         self.k = k
         self.centroids = data[randomIndices, :]
         self.assignClusters(X)
+
+    # TODO: generate data cells from current clusters (eliminate unneeded arg in displayClusters)
+
+    def assignClusters(self, X):
+        newClusters = [list() for _ in range(self.k)]
+
+        for x in X:
+            deltas = [x - m for m in self.centroids]
+            norms = [np.linalg.norm(d) for d in deltas]
+            nearestCluster = np.argmin(norms)
+            newClusters[nearestCluster].append(x)
+
+        self.clusters = newClusters
