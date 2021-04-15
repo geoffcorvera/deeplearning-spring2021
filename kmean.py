481603cc2707db35b0199d96f84d30a507ef3543
Geoffrey Corvera <geoffcorvera@gmail.com>
2021-04-14 19:07:24 -0700

    implement kmeans initialize

--- kmean.py
+++ kmean.py
@@ -5,3 +5,12 @@ data = np.genfromtxt('hw1/510_cluster_dataset.txt')
 
 plt.scatter(data[:, :1], data[:, 1:])
 plt.show()
+
+class KMeans(object):
+
+    def __init__(self, X, k):
+        randomIndices = np.random.choice(X.shape[0], size=k, replace=False)
+
+        self.k = k
+        self.centroids = data[randomIndices, :]
+        self.assignClusters(X)
