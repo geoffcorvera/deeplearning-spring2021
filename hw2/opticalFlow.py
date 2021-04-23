6fb1e3ef91d9576fd73ec7fd4262a8dde1eb3156
Geoff Corvera <geoffcorvera@gmail.com>
2021-04-23 12:41:45 -0700

    import images and test out scipy 2D convolution

--- hw2/opticalFlow.py
+++ hw2/opticalFlow.py
@@ -11,7 +11,30 @@ gy = np.array([(1, 2, 1),
                (0, 0, 0),
                (-1, -2, -1)])
 
+# %%
+# Import pair of images (Note: examples are already single-channel)
+frame1a = image.imread('frame1_a.png')
+frame2a = image.imread('frame2_a.png')
+assert frame1a.shape == frame2a.shape
+
+Ix = signal.convolve2d(frame1a, gx, boundary='symm', mode='same')
+Iy = signal.convolve2d(frame1a, gy, boundary='symm', mode='same')
+
+It = frame2a - frame2a
+
+# %%
 import matplotlib.pyplot as plt 
+
+fig, (ax_orig, ax_gx, ax_gy) = plt.subplots(3, 1, figsize=(6, 15))
+ax_orig.imshow(frame1a)
+ax_orig.set_title('Original')
+ax_orig.set_axis_off()
+ax_gx.imshow(Ix)
+ax_gx.set_title('Ix')
+ax_gx.set_axis_off()
+ax_gy.imshow(Iy)
+ax_gy.set_title('Iy')
+ax_gy.set_axis_off()
 
 # Identify Inputs
 ## pair of M x N images
