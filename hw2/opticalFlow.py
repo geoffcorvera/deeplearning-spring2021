f0a6ef380bba88689eeb4a06364dcf4e294391c4
Geoff Corvera <geoffcorvera@gmail.com>
2021-04-23 08:59:43 -0700

    Adding notes: identify inputs and outputs in comments

--- hw2/opticalFlow.py
+++ hw2/opticalFlow.py
@@ -1,7 +1,15 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-# Identify inputs
+# Identify Inputs
+## pair of M x N images
+
+# Convert images to grayscale
+# 
 
 
-# Identify outputs
+
+# Identify Outputs
+## Vx (M x N): x-component of optical flow for each pixel of the original image
+## Vy (M x N): y-component of optical flow for each px of the original image
+## sqrt(Vx**2 + Vy**2) (M x N): kinda like sobel combination of x & y components
